5785544a7fd12924083f41c1453f4a08546a2c26
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-02-06 21:11:56 +0530

    about me section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-
 import HeroBanner from "./components/HeroBanner";
+import AboutMe from "./components/AboutMe";
 
 function App() {
     return (
         <div className="App">
             <HeroBanner />
+            <AboutMe />
         </div>
     );
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/indian-flag.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+
+const AboutMe = () => {
+    return (
+        <div className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden">
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <img className="sec-2-p-e-1" src={pe1} />
+            <img className="sec-2-p-e-2" src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <div className="text-[40px] md:text-[90px] 2xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>A Product Designer</span>
+                    <span className="flex items-center gap-2">
+                        <span>Based</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In India</span>
+                    </span>
+                </div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it
+                </div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it
+                </div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled itLorem Ipsum is
+                    simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it
+                </div>
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
