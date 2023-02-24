4592cdb8a515ec54cafacb21688988edb1d519eb
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-02-24 20:35:33 +0530

    spacing issue fixed

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Wrapper = ({ children }) => {
     return (
-        <div className="max-w-[1600px] px-5 md:px-10 2xl:px-0 mx-auto pt-[1px] mt-[-1px]">
+        <div className="max-w-[1680px] px-5 md:px-10 mx-auto pt-[1px] mt-[-1px]">
             {children}
         </div>
     );
